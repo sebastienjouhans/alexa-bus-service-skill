d1cac69a00913651c5630bed34c7b7de305ce9a8
sebastien jouhans <sebastien.jouhans@gmail.com>
2016-12-12 22:15:41 +0000

    keep session open on launch skill

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -5,7 +5,7 @@ var registerEventHandlers = function (eventHandlers, skillContext) {
     };
 
     eventHandlers.onLaunch = function (launchRequest, session, response) {
-        response.tell("Welcome to the bus service skill. You can say for example, when is my next bus to Canada Water or London Bridge");
+        response.ask("Welcome to the bus service skill. You can say for example, when is my next bus to Canada Water or London Bridge");
     };
 };
 exports.register = registerEventHandlers;
