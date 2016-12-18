a02c7a7c343b8423b3f834f3f98f78fdf27d836a
sebastien jouhans <sebastien.jouhans@gmail.com>
2016-12-18 20:17:37 +0000

    removed comment

--- src/busService.js
+++ src/busService.js
@@ -3,13 +3,10 @@ var AlexaSkill = require('./AlexaSkill'),
     eventHandlers = require('./eventHandlers'),
     intentHandlers = require('./intentHandlers');
 
-var APP_ID = "amzn1.ask.skill.05fd8b53-71ed-424c-a10e-f14879d37f0b";//replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = "amzn1.ask.skill.05fd8b53-71ed-424c-a10e-f14879d37f0b";
 var skillContext = {};
 
 /**
- * ScoreKeeper is a child of AlexaSkill.
- * To read more about inheritance in JavaScript, see the link below.
- *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
 var BusService = function () {
