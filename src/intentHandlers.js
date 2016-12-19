2bbbe1f97b7149f4d06ada446278dedb44f26f60
sebastien jouhans <sebastien.jouhans@gmail.com>
2016-12-19 23:12:33 +0000

    added try catch  in parsing method

--- src/intentHandlers.js
+++ src/intentHandlers.js
@@ -246,20 +246,28 @@ var getRequest = function(url, eventCallback, errorCallback) {
 
 var parseBuseResponse = function(body) {
     var buses = [];
-    var data = JSON.parse(body);
-    //console.log(body);
-    for(var i=0; i<data.length; i++)
+    try
     {
-        var bus = {};
-        bus.timeToStation = data[i].timeToStation;
-        bus.lineName = data[i].lineName;
-        bus.direction = data[i].towards;
-        var time = parseInt(bus.timeToStation);
-        bus.minutes = Math.floor(time / 60);
-        bus.seconds = time - bus.minutes * 60;
-        buses.push(bus);
+        console.log("parseBuseResponse body = "+body);
+        var data = JSON.parse(body);
+        for(var i=0; i<data.length; i++)
+        {
+            var bus = {};
+            bus.timeToStation = data[i].timeToStation;
+            bus.lineName = data[i].lineName;
+            bus.direction = data[i].towards;
+            var time = parseInt(bus.timeToStation);
+            bus.minutes = Math.floor(time / 60);
+            bus.seconds = time - bus.minutes * 60;
+            buses.push(bus);
+        }
+        return buses;
     }
-    return buses;
+    catch(error)
+    {
+        console.log("error parseBuseResponse body = "+body);
+        return buses;
+    }
 };
 
 
