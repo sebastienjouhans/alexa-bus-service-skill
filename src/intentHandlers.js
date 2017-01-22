a7454039dd9d4593d617ffe91f33c3f6e7340e05
sebastien jouhans <sebastien.jouhans@gmail.com>
2017-01-22 14:59:35 +0000

    added help card

--- src/intentHandlers.js
+++ src/intentHandlers.js
@@ -4,14 +4,14 @@ var BusIntent = require('./BusIntent');
 
 
 var registerIntentHandlers = function (intentHandlers, skillContext) {
-  intentHandlers.NextBusToIntent = function (intent, session, response) {
+  intentHandlers.NextBusesToIntent = function (intent, session, response) {
     try {
       let busIntent = new BusIntent();
       let busDirection = busIntent.getBusDirection(intent);
-      console.log('## NextBusToIntent ' + busDirection);
+      console.log('## NextBusesToIntent ' + busDirection);
       if (busDirection == null) {
         response.ask("Sorry I didn't reconize the destination, please try again.");
-        console.log('## NextBusToIntent - did not reconize the destination');
+        console.log('## NextBusesToIntent - did not reconize the destination');
         return;
       }
       busIntent.getBuses(busDirection, response);
@@ -21,9 +21,9 @@ var registerIntentHandlers = function (intentHandlers, skillContext) {
     }
   };
 
-  intentHandlers.NextBusIntent = function (intent, session, response) {
+  intentHandlers.NextBusesIntent = function (intent, session, response) {
     try {
-      console.log('## NextBusIntent');      
+      console.log('## NextBusesIntent');      
       let busIntent = new BusIntent();
       busIntent.getBuses('all', response);
     } catch (error) {
@@ -34,16 +34,24 @@ var registerIntentHandlers = function (intentHandlers, skillContext) {
   };
 
   intentHandlers['AMAZON.HelpIntent'] = function (intent, session, response) {
-    response.ask('You can say for example, when is the next bus to Canada Water. You also can replace Canada Water with another supported directions such as London Bridge, Bermondsey or all. Using all will give buses for all directions.');
+    let speechOutput = 'You can say for example, when is the next bus to Canada Water. You also can replace Canada Water with another supported directions such as London Bridge, Bermondsey or all. Using all will give buses for all directions.';
+    let cardTitle = 'Help';
+    let cardContent = '- Alexa ask bus service when is the next bus to Canada Water\n' +
+                      '- Alexa ask bus service when is the next bus to London Bridge\n' +
+                      '- Alexa ask bus service when is the next bus to all\n' +
+                      '- Alexa ask bus service when is the next bus\n';
+    response.tellWithCard(speechOutput,
+                          cardTitle,
+                          cardContent);
   };
 
   intentHandlers['AMAZON.CancelIntent'] = function (intent, session, response) {
-    var speechOutput = 'Goodbye';
+    let speechOutput = 'Goodbye';
     response.tell(speechOutput);
   };
 
   intentHandlers['AMAZON.StopIntent'] = function (intent, session, response) {
-    var speechOutput = 'Goodbye';
+    let speechOutput = 'Goodbye';
     response.tell(speechOutput);
   };
 };
